f5885bd9318c8f6b1af9cf8a93966884d0d47a52
TheEt1234 <74461678+TheEt1234@users.noreply.github.com@users.noreply.github.com>
2023-06-10 12:36:55 +0200

    fixed unit build being bad

--- scripts/unitBuild.js
+++ scripts/unitBuild.js
@@ -3,14 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.validateUnitBuild = void 0;
 var players_1 = require("./players");
 var ranks_1 = require("./ranks");
+var procs = {
+    "400": true,
+    "401": true,
+    "402": true, //hyperProcessor
+};
 var ActionType = Packages.mindustry.net.Administration.ActionType;
 var validateUnitBuild = function () {
     Vars.netServer.admins.addActionFilter(function (action) {
-        if (action.type == ActionType.configure) { //fires on logic build too
+        if (action.type == ActionType.configure) { // UPDATE: does NOT fire on logic build
             var player_1 = action.player;
             var tile_1 = action.tile;
             Core.app.post(function () {
-                //explenation: the config gives you bullshit bytes, which i can't figure out how to convert to a string, so i just use this scuffed method of getting the future tile
+                //explenation: the config gives you bytes, which i can't figure out how to convert to a string, so i just use this scuffed method of getting the future tile
                 try {
                     validateProcessor(tile_1, player_1);
                 }
@@ -20,6 +25,25 @@ var validateUnitBuild = function () {
         }
         return true;
     });
+    Events.on(EventType.BlockBuildEndEvent, function (e) {
+        try {
+            var breaking = e.breaking, tile = e.tile, unit = e.unit;
+            var block = tile.block();
+            if (breaking)
+                return;
+            //@ts-ignore
+            if (!procs[block.id])
+                return;
+            if (!unit.isPlayer()) {
+                setNetAir(tile);
+                return;
+            }
+            validateProcessor(tile, unit.player);
+        }
+        catch (e) {
+            Log.info(e);
+        }
+    });
 };
 exports.validateUnitBuild = validateUnitBuild;
 function validateProcessor(tile, originalPlayer) {
@@ -28,6 +52,9 @@ function validateProcessor(tile, originalPlayer) {
     var includesBuild = code.includes("ucontrol build");
     if (includesBuild && !fishPlayer.ranksAtLeast(ranks_1.Rank.trusted)) {
         originalPlayer.sendMessage("Hey hey, you're trying to place logic which contains unit build, you need to gain trusted to do that");
-        tile.setNet(Blocks.air);
+        setNetAir(tile);
     }
+}
+function setNetAir(tile) {
+    Core.app.post(function () { tile.setNet(Blocks.air); });
 }
--- scripts/unitBuild.ts
+++ scripts/unitBuild.ts
@@ -1,29 +1,52 @@
 import { FishPlayer } from "./players";
 import { Rank } from "./ranks";
 
+const procs={
+    "400":true, //microProcessor
+    "401":true, //logicProcessor
+    "402":true, //hyperProcessor
+}
 
 const ActionType = Packages.mindustry.net.Administration.ActionType;
 export const validateUnitBuild=()=>{ //past
     Vars.netServer.admins.addActionFilter((action:PlayerAction)=>{
-        if(action.type==ActionType.configure){ //fires on logic build too
+        if(action.type==ActionType.configure){ // UPDATE: does NOT fire on logic build
             const player=action.player
             const tile=action.tile
             Core.app.post(()=>{ 
-//explenation: the config gives you bullshit bytes, which i can't figure out how to convert to a string, so i just use this scuffed method of getting the future tile
+//explenation: the config gives you bytes, which i can't figure out how to convert to a string, so i just use this scuffed method of getting the future tile
                 try{validateProcessor(tile,player)}catch(e){}
             })
             return true
         }
         return true
     })
+    
+    Events.on(EventType.BlockBuildEndEvent, (e)=>{
+        try{
+            const { breaking, tile, unit } = e;
+            const block=tile.block();
+            if(breaking) return;
+            //@ts-ignore
+            if(!procs[block.id]) return;
+            if(!unit.isPlayer()) { setNetAir(tile); return }
+            validateProcessor(tile, unit.player)
+        } catch(e:any){
+            Log.info(e)
+        }
+    })
 }
 
-function validateProcessor(tile:Tile,originalPlayer:mindustryPlayer ){ // future
+function validateProcessor(tile:Tile,originalPlayer:mindustryPlayer){ // future
     const fishPlayer=FishPlayer.get(originalPlayer)
     const code=tile.build.code
     const includesBuild=code.includes("ucontrol build")
-    if(includesBuild&&!fishPlayer.ranksAtLeast(Rank.trusted)){
+    if(includesBuild &&  !fishPlayer.ranksAtLeast(Rank.trusted)){
         originalPlayer.sendMessage("Hey hey, you're trying to place logic which contains unit build, you need to gain trusted to do that")
-        tile.setNet(Blocks.air)
+        setNetAir(tile)
     }
+}
+
+function setNetAir(tile:Tile){
+    Core.app.post(()=>{tile.setNet(Blocks.air)})
 }
